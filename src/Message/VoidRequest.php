43981778d331f34acf35b8b870ad2cb0f464e2f5
nikolaymikov <nikolay.mikov@ampeco.global>
2022-04-18 15:50:18 +0300

    add: retrieve transaction request and  void request

--- src/Message/VoidRequest.php
+++ src/Message/VoidRequest.php
@@ -2,6 +2,8 @@
 
 namespace Ampeco\OmnipayPayMob\Message;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class VoidRequest extends AbstractRequest
 {
     public function getEndpoint()
@@ -10,10 +12,47 @@ class VoidRequest extends AbstractRequest
         return "/acceptance/void_refund/void?token={$this->getAuthToken()}";
     }
 
+    public function sendData($data)
+    {
+        $authToken = $this->getAuthToken();
+
+        info('VOID REQUEST');
+
+        $orderData = [
+            'auth_token' => $authToken,
+            'order_id' => $this->getTransactionReference(),
+            'merchant_order_id' => $this->getTransactionId(),
+        ];
+
+        $transactionResponse = $this->httpClient->request(
+            $this->getHttpMethod(),
+            $this->getBaseUrl() . '/ecommerce/orders/transaction_inquiry',
+            $this->getHeaders(),
+            json_encode($orderData),
+        );
+
+        $transactionId = json_decode($transactionResponse->getBody()->getContents(), true)['id'];
+
+        info('VOID REQUEST' . $transactionId);
+
+        $voidData = [
+            'transaction_id' => $transactionId,
+        ];
+
+        $voidResponse = $this->httpClient->request(
+            $this->getHttpMethod(),
+            $this->getBaseUrl() . "/acceptance/void_refund/void?token=$authToken",
+            $this->getHeaders(),
+            json_encode($voidData),
+        );
+
+        return $this->createResponse($voidResponse->getBody()->getContents(), Response::HTTP_OK);
+    }
+
     public function getData()
     {
-        return [
-            'transaction_id' => '',
-        ];
+//        return [
+//            'transaction_id' => '',
+//        ];
     }
 }
