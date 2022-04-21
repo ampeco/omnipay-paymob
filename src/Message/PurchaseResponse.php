25910272b2113b98add4445d5a3734d5b03e729f
nikolaymikov <nikolay.mikov@ampeco.global>
2022-04-21 16:56:40 +0300

    upd:  pre-authorization completed [sc-32144]

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -8,6 +8,10 @@ class PurchaseResponse extends Response
 {
     public function isSuccessful()
     {
+        if ($this->data['is_auth'] === 'true') {
+            return $this->statusCode < 400;
+        }
+
         return parent::isSuccessful() && $this->getCode() === Gateway::STATUS_SUCCESS;
     }
 }
